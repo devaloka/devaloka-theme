8acc04112cd73b6ba38004742a02ef6013332100
Whizark <devaloka@whizark.com>
2015-12-29 21:05:22 +0900

    chore: bump version to 0.6.2

--- devaloka-theme.php
+++ devaloka-theme.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Theme
 Description: Provides Interfaces, Traits, and Classes for WordPress Theme
-Version: 0.6.1
+Version: 0.6.2
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
